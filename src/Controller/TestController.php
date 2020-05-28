cd5c900c848831f1cad7de22b55c59e3c1bf01e8
user-penguin <kobzev96@gmail.com>
2020-05-29 04:05:23 +0700

    create test /getAllUset route

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,7 +4,9 @@
 namespace App\Controller;
 
 
+use App\Repository\UserRepository;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class TestController
 {
@@ -14,5 +16,19 @@ class TestController
             'author' => 'debik',
             'version' => '0.-1.-2',
         ]);
+    }
+
+    public function getUsers(UserRepository $userRepository) : Response
+    {
+        $users = $userRepository->getAllUser();
+        $serObj = [
+            "users" => [],
+        ];
+        foreach ($users as $value) {
+            $serObj["users"][] = $value["name"];
+        }
+        $resp = new Response(json_encode ($serObj,JSON_UNESCAPED_UNICODE));
+        $resp->headers->set('Content-Type', 'application/json');
+        return $resp;
     }
 }
