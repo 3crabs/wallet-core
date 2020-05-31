8e6b9fc1f59eb37e278c3651cf752c6129799ca5
user-penguin <kobzev96@gmail.com>
2020-05-31 16:54:02 +0700

    add get categories stub

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,8 +4,11 @@
 namespace App\Controller;
 
 
+use App\Entity\User;
 use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class TestController
@@ -30,5 +33,32 @@ class TestController
         $resp = new Response(json_encode ($serObj,JSON_UNESCAPED_UNICODE));
         $resp->headers->set('Content-Type', 'application/json');
         return $resp;
+    }
+
+    public function addUser(EntityManagerInterface $entityManager, Request $request) : Request
+    {
+//        $name = $request->query->get('name');
+        return $request;
+//        $user = new User();
+//        $user->setName($name);
+//        $entityManager->persist($user);
+//        $entityManager->flush();
+//
+//        return new JsonResponse([
+//            "id" => $user->getId(),
+//        ]);
+    }
+
+    // get all categories stub
+    public function getCategories() : JsonResponse
+    {
+        return new JsonResponse([
+            "0" => "Авто",
+            "1" => "Хозяйственые товары",
+            "2" => "Продукты",
+            "3" => "Здоровье",
+            "4" => "Спорт",
+            "5" => "Наркотики",
+        ]);
     }
 }
